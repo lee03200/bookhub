b1718d4d8dc4edeb0b91f300b768a6722cb8b654
lee03200 <1439614452@qq.com>
2025-11-26 15:45:06 +0800

    Create BookDetail.vue

---
+++ src/views/BookDetail.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1>书籍详情</h1>
+    <p>当前书籍 ID: {{ $route.params.id }}</p>
+    <button @click="$router.back()">返回</button>
+  </div>
+</template>
