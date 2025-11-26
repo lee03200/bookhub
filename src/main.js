9fdf0c129c4a35403042b6a33042dcb914cf1e54
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-26 15:04:33 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
-import './assets/css/global.css'
 
 const app = createApp(App)
 const pinia = createPinia()
