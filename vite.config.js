75df7ffc5dd44ecf0580fd1287e5c4e89c83fee3
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-29 00:57:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/bookhub/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') 
+    }
+  },
   server: {
     port: 8080,
     proxy: {
