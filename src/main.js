fb54d9645bfc03635b539187dbef6468c5b6c09b
lee03200 <1439614452@qq.com>
2025-11-28 23:11:55 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
 
+// 引入全局样式
+import './assets/styles/reset.css'
+import './assets/styles/variables.css'
+import './assets/styles/fonts.css'
+
 const app = createApp(App)
 const pinia = createPinia()
 
