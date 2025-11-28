e05a92ce9b708e5371bcf8414871568e83338d3c
ravizhan <ravizhan@hotmail.com>
2025-11-28 19:00:52 +0800

    fix(styles): 修复样式异常 chore(package): 添加Tailwind CSS依赖

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // 新增：引入path模块（Node.js内置，无需额外安装）
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   server: {
     port: 3000,
     open: true
