3e886497e67e7cd5bf6a4a19b32e0b1ff47728de
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-29 04:40:05 +0800

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -211,14 +211,7 @@ const toggleFavorite = (book) => {
 }
 </script>
 
-<style scoped>
-.btn-primary {
-  @apply px-6 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition;
-}
-.btn-outline {
-  @apply px-6 py-2 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-50 transition;
-}
-</style>
+
 
 
 
