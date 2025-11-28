19b7e9b58c5c3ccf9dfdc9302c9a5e53a0481c6e
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-29 03:49:52 +0800

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -185,7 +185,8 @@ const myShelfIds = computed(() => myShelfBooks.value.map(b => b.id))
 const readingStats = computed(() => bookStore.readingStats)
 
 // 推荐 & 热门（确保有默认值）
-const recommendedBooks = computed(() => bookStore.recommended || [])
+import { recommendedBooksData } from '@/data/books'
+const recommendedBooks = computed(() => bookStore.recommended || recommendedBooksData)
 const popularBooks = computed(() => bookStore.popular || [])
 
 // 分类浏览
@@ -215,4 +216,5 @@ const toggleFavorite = (book) => {
   @apply px-6 py-2 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-50 transition;
 }
 </style>
+
 
