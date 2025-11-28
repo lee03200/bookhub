1c47b760c958252f44bee9e58ef167f2cfed80e1
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-28 23:38:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // 新增：引入path模块（Node.js内置，无需额外安装）
+import tailwind from 'vite-plugin-tailwind'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bookhub/',
-  plugins: [vue()],
+  plugins: [vue(),tailwind()],
   server: {
     port: 3000,
     open: true
