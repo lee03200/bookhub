f926887824d0eb6039bcf36a0de7c7512aab8943
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-29 04:00:46 +0800

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -173,6 +173,7 @@ import { ref, computed } from 'vue'
 import { useUserStore } from '@/stores/user'
 import { useBookStore } from '@/stores/books'
 import BookCard from '@/components/BookCard.vue'
+import { recommendedBooksData } from '@/data/books' 
 
 // Stores
 const userStore = useUserStore()
@@ -216,5 +217,6 @@ const toggleFavorite = (book) => {
   @apply px-6 py-2 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-50 transition;
 }
 </style>
+
 
 
