a4e6cdb3fea8ec4d54b83de3ae3ac6b43ca73039
lee03200 <1439614452@qq.com>
2025-11-28 23:36:47 +0800

    Update BookDetail.vue

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="container mx-auto p-4">
-    <h1>书籍详情</h1>
-    <p>当前书籍 ID: {{ $route.params.id }}</p>
-    <button @click="$router.back()">返回</button>
+    <button @click="$router.back()" class="btn btn-primary mb-4">返回</button>
+    <div v-if="book" class="flex flex-col md:flex-row gap-6">
+      <img :src="book.cover" alt="book.title" class="w-64 h-auto mx-auto md:mx-0">
+      <div>
+        <h1 class="text-3xl font-bold">{{ book.title }}</h1>
+        <p class="text-gray-600">作者: {{ book.author }}</p>
+        <p class="mt-4">评分: {{ book.rating }}</p>
+        <button 
+          class="btn btn-primary mt-4"
+          @click="addToShelf(book)"
+        >
+          添加到书架
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useBookStore } from '@/stores/books'
+
+const route = useRoute()
+const bookStore = useBookStore()
+const book = ref(null)
+
+onMounted(() => {
+  // 实际项目中应调用API获取详情
+  const mockBooks = [/* 书籍数据 */]
+  book.value = mockBooks.find(b => b.id === Number(route.params.id))
+})
+
+function addToShelf(book) {
+  bookStore.toggleFavorite(book)
+}
+</script>
