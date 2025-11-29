6970f44312ca4029feba845dc508a7144d6724ff
czj918666273-tech1 <czj918666273@icloud.com>
2025-11-29 11:31:23 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"  // ← 必须包含 .vue！
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
